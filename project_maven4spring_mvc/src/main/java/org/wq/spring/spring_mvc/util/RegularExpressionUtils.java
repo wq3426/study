e253cebfa762a588360b99d42e1b30fd470e788f
0-PC\wq3426 <342651808@qq.com>
2017-06-19 19:15:33 +0800

    添加正则表达式工具类

---
+++ project_maven4spring_mvc/src/main/java/org/wq/spring/spring_mvc/util/RegularExpressionUtils.java
@@ -0,0 +1,59 @@
+package org.wq.spring.spring_mvc.util;
+
+import java.util.regex.Pattern;
+
+/**
+ * @Description 正则表达式校验工具类
+ *
+ * @createTime 2016年10月27日 下午7:40:53
+ *
+ * @author <a href="mailto:wangqiang@trans-it.cn">wangqiang</a>
+ */
+public class RegularExpressionUtils {
+	/**
+	 * 相关资料链接：
+	 * 日期正则  http://blog.csdn.net/lxcnn/article/details/4362500
+	 */
+	
+	/**
+	 * 正整数     ^\\d+$
+	 * 浮点数     ^(-?\\d+)(\\.\\d+)?$
+	 * 车牌号     ^[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z]{1}[A-Z]{1}[警京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼]{0,1}[A-Z0-9]{4}[A-Z0-9挂学警港澳]{1}$
+	 * 身份证号码  ^[1-9]{1}[0-9]{14}$|^[1-9]{1}[0-9]{16}([0-9]|[xX])$
+	 * 日期格式  yyyy-MM-dd 或 yyyy-M-d ^[0-9]{4}-(0?[0-9]|1[0-2])-(0?[1-9]|[12]?[0-9]|3[01])$
+	 */
+	public static final String intPattern = "^\\d+$";
+	
+	public static final String doublePattern = "^(-?\\d+)(\\.\\d+)?$";
+	
+	public static final String carplatePattern = "^[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z]{1}[A-Z]{1}[警京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼]{0,1}[A-Z0-9]{4}[A-Z0-9挂学警港澳]{1}$";
+	
+	public static final String identityIDPattern = "^[1-9]{1}[0-9]{14}$|^[1-9]{1}[0-9]{16}([0-9]|[xX])$";
+	
+	public static final String y_M_d_DatePattern = "^[0-9]{4}-(0?[0-9]|1[0-2])-(0?[1-9]|[12]?[0-9]|3[01])$";
+	
+	public static final String mobilePattern = "^((\\d{3}-\\d{8}|\\d{4}-\\d{7,8})|(1[3|5|7|8][0-9]{9}))$";
+	
+	//yyyy-MM-dd HH:mm:ss
+	public static final String timePattern = "(\\d{2}|\\d{4})(?:\\-)?([0]{1}\\d{1}|[1]{1}[0-2]{1})(?:\\-)?([0-2]{1}\\d{1}|[3]{1}[0-1]{1})(?:\\s)?([0-1]{1}\\d{1}|[2]{1}[0-3]{1})(?::)?([0-5]{1}\\d{1})(?::)?([0-5]{1}\\d{1})";
+	
+	/**
+	 * @description 校验正则表达式
+	 * @date 2016年10月28日 上午10:27:59
+	 * @param pattern
+	 * @return
+	 */
+	public static boolean matches(String matchStr, String patternStr){
+		Pattern pattern = Pattern.compile(patternStr);
+		
+		return pattern.matcher(matchStr).matches();
+	}
+	
+	public static void main(String[] args) {  
+//		Pattern pattern = Pattern.compile("^[0-9]{4}-(0?[0-9]|1[0-2])-(0?[1-9]|[12]?[0-9]|3[01])$");
+//		boolean match = pattern.matcher("2424-12-14").matches();
+//		System.out.println(match);
+		System.out.println(matches("琼G88002", carplatePattern));
+	} 
+	
+}
--- project_maven4spring_mvc/src/main/java/validation/Test1.java
+++ project_maven4spring_mvc/src/main/java/validation/Test1.java
@@ -2,7 +2,11 @@
 package validation;
 
 
+import java.util.regex.Pattern;
+
 import javax.xml.bind.ValidationException;
+
+import org.wq.spring.spring_mvc.util.RegularExpressionUtils;
 
 import validation.util.ValidationUtil;
 import validation.vo.Person;
@@ -16,13 +20,14 @@ import validation.vo.Person;
 public class Test1 {
 
 	public static void main(String [] args ){
-		Person person = new Person();
-		try {
-			ValidationUtil.validate(person);
-		} catch (ValidationException e) {
-			System.out.println(e.getMessage());
-		}
+//		Person person = new Person();
+//		try {
+//			ValidationUtil.validate(person);
+//		} catch (ValidationException e) {
+//			System.out.println(e.getMessage());
+//		}
 		
+		System.out.println(RegularExpressionUtils.matches("2012-02-09 23:18:43", RegularExpressionUtils.timePattern));
 	}
 
 }
